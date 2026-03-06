a7a68ff3f7587ce99638dbf47c7fd69350e76eed
Ajay Kumar <ajay.ce.kumar@avanade.com>
2026-03-06 16:58:38 +0100

    adding airports logic

--- src/components/AirportSearch.tsx
+++ src/components/AirportSearch.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Check, ChevronsUpDown } from "lucide-react";
-import { airports } from "@nwpr/airport-codes";
+import { airports } from "aircodes";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
